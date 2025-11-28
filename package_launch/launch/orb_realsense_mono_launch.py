43e7ba61119d8aca4ab4d5d466ed100b9cb7d9d1
Patryk Dudizński <dodpat02@gmail.com>
2025-11-28 21:12:10 +0100

    format

--- package_launch/launch/orb_realsense_mono_launch.py
+++ package_launch/launch/orb_realsense_mono_launch.py
@@ -1,28 +1,48 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import ament_index_python.packages
+
+import os
+
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='package_launch',
-            executable='cam2img',
-            name='cam2img',
-            output='screen',
-        ),
-        Node(
-            package='orbslam3',
-            executable='mono',
-            name='mono',
-            output='screen',
-            arguments=[
-                '/home/robo-dude/projects/ros2_ws/src/orbslam3_ros2/vocabulary/ORBvoc.txt',
-                '/home/robo-dude/projects/ros2_ws/src/orbslam3_ros2/config/monocular/EuRoC.yaml',
-            ],
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_to_camera_tf',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'camera_link']
-        )
-    ])
+  config_settings = os.path.join( ament_index_python.packages.get_package_share_directory('orbslam3'),
+    'config','monocular','EuRoC.yaml')
+  
+  config_voc = os.path.join( ament_index_python.packages.get_package_share_directory('orbslam3'),
+    'vocabulary','ORBvoc.txt')
+  
+  
+
+  # params = os.path.join(config_directory, 'zed_f9p.yaml')
+
+  return LaunchDescription([
+      # Node(
+      #     package='package_launch',
+      #     executable='cam2img',
+      #     name='cam2img',
+      #     output='screen',
+      # ),
+      Node(
+          package='orbslam3',
+          executable='mono',
+          name='mono',
+          output='screen',
+          arguments=[
+              # config_voc,
+              "/home/lemonx/it/nomad_software/nomad_core/ROS2_ORB-SLAM3_Odometry/orbslam3_ros2/vocabulary/ORBvoc.txt",
+              "/home/lemonx/it/nomad_software/nomad_core/ROS2_ORB-SLAM3_Odometry/orbslam3_ros2/config/monocular/EuRoC.yaml",
+              # config_settings,
+          ],
+          remappings=[
+              ('/camera', '/camera_left/image_raw'),
+              ('/odom', '/odom_camera'),
+          ]
+      )
+      # Node(
+      #     package='tf2_ros',
+      #     executable='static_transform_publisher',
+      #     name='base_to_camera_tf',
+      #     arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'camera_link']
+      # )
+  ])
