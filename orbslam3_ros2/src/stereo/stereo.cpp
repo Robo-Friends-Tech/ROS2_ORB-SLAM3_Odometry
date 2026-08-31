2a20bc1687ded261af5a18d43de603de51effd10
Patryk Dudzinski <dudzinski.patryk.02@gmail.com>
2026-08-31 21:16:03 +0200

    added param for vizualization

--- orbslam3_ros2/src/stereo/stereo.cpp
+++ orbslam3_ros2/src/stereo/stereo.cpp
@@ -1,34 +1,39 @@
-#include<iostream>
-#include<algorithm>
-#include<fstream>
-#include<chrono>
-
 #include "rclcpp/rclcpp.hpp"
 #include "stereo-slam-node.hpp"
+#include <algorithm>
+#include <cctype>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 #include "System.h"
 
-int main(int argc, char **argv)
+int
+main(int argc, char** argv)
 {
-    if(argc < 4)
-    {
-        std::cerr << "\nUsage: ros2 run orbslam stereo path_to_vocabulary path_to_settings do_rectify" << std::endl;
-        return 1;
-    }
+  if (argc < 4) {
+    std::cerr
+      << "\nUsage: ros2 run orbslam stereo path_to_vocabulary path_to_settings do_rectify"
+      << std::endl;
+    return 1;
+  }
 
-    rclcpp::init(argc, argv);
+  rclcpp::init(argc, argv);
 
-    // malloc error using new.. try shared ptr
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+  std::string vis_arg = argv[5];
+  std::transform(vis_arg.begin(), vis_arg.end(), vis_arg.begin(), [](unsigned char c) {
+    return std::tolower(c);
+  });
+  bool visualization = (vis_arg == "true" || vis_arg == "1");
 
-    bool visualization = true;
-    ORB_SLAM3::System pSLAM(argv[1], argv[2], ORB_SLAM3::System::STEREO, visualization);
+  ORB_SLAM3::System pSLAM(argv[1], argv[2], ORB_SLAM3::System::STEREO, visualization);
 
-    auto node = std::make_shared<StereoSlamNode>(&pSLAM, argv[2], argv[3]);
-    std::cout << "============================ " << std::endl;
+  auto node = std::make_shared<StereoSlamNode>(&pSLAM, argv[2], argv[3]);
+  std::cout << "============================ " << std::endl;
 
-    rclcpp::spin(node);
-    rclcpp::shutdown();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
 
-    return 0;
+  return 0;
 }
